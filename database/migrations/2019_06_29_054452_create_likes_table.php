713cc02e8fe4693fb22aa2220eb91782b250cf09
thehao <chungthehao@gmail.com>
2019-06-29 13:18:35 +0700

    Creating migration, model, factory and controlller.

---
+++ database/migrations/2019_06_29_054452_create_likes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('reply_id'); // like cho câu trả lời nào?
+            $table->integer('user_id'); // ai là ng đã like?
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
