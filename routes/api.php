1a3fe65e23fc3ae7ec1a749bdaacab20f7bd8116
thehao <chungthehao@gmail.com>
2019-06-30 18:40:06 +0700

    Like and unlike for a reply

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ use Illuminate\Http\Request;
 Route::apiResource('/questions', 'QuestionController');
 Route::apiResource('/categories', 'CategoryController');
 Route::apiResource('/questions.replies', 'ReplyController');
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unlikeIt');
